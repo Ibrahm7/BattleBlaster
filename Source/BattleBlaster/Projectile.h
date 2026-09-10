8038e6f91d39ae6ce2f46938401c925eb5390937
Ibrahim Yurdakul <ibrahimyurdakul.dev@gmail.com>
2026-09-10 15:57:51 +0300

    Added Hit Events

--- Source/BattleBlaster/Projectile.h
+++ Source/BattleBlaster/Projectile.h
@@ -30,4 +30,7 @@ public:
 
 	UPROPERTY(VisibleAnywhere)
 	UProjectileMovementComponent* ProjectileMovementComp;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 };
