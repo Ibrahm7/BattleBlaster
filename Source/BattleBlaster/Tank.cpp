4112b43fc37ee1c2186ec4a91ffb97ce0808a7ac
Ibrahim Yurdakul <ibrahimyurdakul.dev@gmail.com>
2026-09-10 13:55:32 +0300

    first commit

---
+++ Source/BattleBlaster/Tank.cpp
@@ -0,0 +1,76 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Camera/CameraComponent.h"
+#include "InputMappingContext.h"
+#include "Kismet/GameplayStatics.h"
+#include "Tank.h"
+
+ATank::ATank(){
+    SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+    SpringArmComp->SetupAttachment(CapsuleComp);
+    CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+    CameraComp->SetupAttachment(SpringArmComp);
+}
+
+
+// Called when the game starts or when spawned
+void ATank::BeginPlay()
+{
+	Super::BeginPlay();
+
+    if(APlayerController* PlayerController = Cast<APlayerController>(Controller)){
+        if(ULocalPlayer* LocalPlayer = PlayerController->GetLocalPlayer()){
+            UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer);
+            if(Subsystem){
+                Subsystem->AddMappingContext(DefaultMappingContext,0);
+            }
+        }
+    }
+	
+}
+
+// Called every frame
+void ATank::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+    APlayerController* PlayerController = Cast<APlayerController>(GetController());
+
+    if(PlayerController){
+        FHitResult HitResult;
+        PlayerController->GetHitResultUnderCursor(ECC_Visibility,false,HitResult);
+        RotateTurret(HitResult.ImpactPoint);
+        // DrawDebugSphere(GetWorld(),HitResult.ImpactPoint,25.0f,12,FColor::Red);
+    }
+}
+
+// Called to bind functionality to input
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+    if(EIC){
+        EIC->BindAction(MoveAction,ETriggerEvent::Triggered,this,&ATank::MoveInput);
+        EIC->BindAction(TurnAction,ETriggerEvent::Triggered,this,&ATank::TurnInput);
+        EIC->BindAction(FireAction,ETriggerEvent::Started,this,&ATank::Fire);
+    }
+}
+
+void ATank::MoveInput(const FInputActionValue& Value)
+{
+    float InputValue = Value.Get<float>();
+
+    FVector DeltaLocation = FVector(0.0f,0.0f,0.0f);
+    DeltaLocation.X = speed * InputValue * UGameplayStatics::GetWorldDeltaSeconds(GetWorld());
+    AddActorLocalOffset(DeltaLocation,true);
+}
+
+void ATank::TurnInput(const FInputActionValue &Value)
+{
+    float InputValue = Value.Get<float>();
+
+    FRotator DeltaRotation = FRotator(0.0f,0.0f,0.0f);
+    DeltaRotation.Yaw = TurnRate * InputValue * GetWorld()->GetDeltaSeconds();
+    AddActorLocalRotation(DeltaRotation,true);
+}
