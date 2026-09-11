36481d9694185a6df6ba1e235d5e33a446799ddd
Ibrahim Yurdakul <ibrahimyurdakul.dev@gmail.com>
2026-09-11 20:16:24 +0300

    Changed The cursor and hided when we died

--- Source/BattleBlaster/Tank.cpp
+++ Source/BattleBlaster/Tank.cpp
@@ -89,9 +89,11 @@ void ATank::SetPlayerEnabled(bool Enabled)
     if(PlayerController){
         if(Enabled){
             EnableInput(PlayerController);
+            
         }else{
             DisableInput(PlayerController);
         }
+        PlayerController->bShowMouseCursor = Enabled;
     }
 }
 
